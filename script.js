38775ddbedca23837c685ae2bc9bc9c9369f6130
Alexey Akulich <loki1987by@yandex.ru>
2021-09-19 18:11:14 +0300

    feat: add mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navigation = document.querySelector('nav')
 const avatar = document.querySelector('.avatar')
 const code = document.querySelector('h5')
+const menu = document.querySelector('.menu')
+const nav = document.querySelector('nav')
 
 const rand = () => {
   return Math.floor(Math.random() * 4)
@@ -33,6 +35,20 @@ function resetAnimation() {
   avatar.src = '/assets/avatar.jpg'
 }
 
+function closeMenu() {
+  nav.classList.remove('asyde')
+  menu.removeAttribute('style')
+  menu.addEventListener('click', openMenu)
+  menu.removeEventListener('click', closeMenu)
+}
+
+function openMenu() {
+  nav.classList.add('asyde')
+  menu.setAttribute('style', 'background-image: url("./assets/close.svg")')
+  menu.removeEventListener('click', openMenu)
+  menu.addEventListener('click', closeMenu)
+}
+
 setInterval(() => {
   code.setAttribute('style', `text-shadow: ${rand()}px ${rand()}px ${rand()}px ${shadowColor()}`)
 }, 2500);
@@ -42,9 +58,10 @@ avatar.addEventListener('mouseout', resetAnimation)
 navigation.children[0].addEventListener('click', (e) => {
   const text = e.target.textContent.toLowerCase()
   const target = document.getElementById(text)
+  console.log(target.style)
   target.setAttribute('style', 'background-color: yellow')
   setTimeout(() => {
     target.removeAttribute('style')
   }, 3000);
-  console.log(target)
 })
+menu.addEventListener('click', openMenu)
