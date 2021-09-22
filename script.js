918272910fe9cde9168a38692a60b9aea3d34df1
Alexey Akulich <loki1987by@yandex.ru>
2021-09-22 23:04:14 +0300

    fix: fix review mistakes

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-const navigation = document.querySelector('.navigation')
-const avatar = document.querySelector('.avatar')
-const menu = document.querySelector('.menu')
-const code = document.querySelector('.javascript')
+const navigation = document.querySelector(".navigation");
+const avatar = document.querySelector(".avatar");
+const menu = document.querySelector(".menu");
+const code = document.querySelector(".javascript");
 
+let brInserted = false;
 
-if(window.innerWidth < 600){
-  const codeAttributes = document.querySelector('.hljs-number')
-  const br = document.createElement('br')
-  code.insertBefore(br, codeAttributes)
-
+function codeChecker() {
+  const codeAttributes = document.querySelector(".hljs-number");
+  if (window.innerWidth < 600 && !brInserted) {
+    const br = document.createElement("br");
+    code.insertBefore(br, codeAttributes);
+    brInserted = true;
+  }
+  if (window.innerWidth > 600) {
+    const br = document.querySelector("br");
+    if (br) {
+      br.remove();
+      brInserted = false;
+    }
+  }
 }
 const rand = () => {
-  return Math.floor(Math.random() * 4)
-}
+  return Math.floor(Math.random() * 4);
+};
 
 const animations = [
-  './assets/cinema.gif',
-  './assets/glitch.gif',
-  './assets/glitch2.gif',
-  './assets/matrix.gif',
-]
+  "./assets/cinema.gif",
+  "./assets/glitch.gif",
+  "./assets/glitch2.gif",
+  "./assets/matrix.gif",
+];
 
-let imagesArray = []
+let imagesArray = [];
 
 for (let i = 0; i < animations.length; i++) {
   let img = new Image();
@@ -32,35 +42,51 @@ for (let i = 0; i < animations.length; i++) {
 }
 
 function changeAvatar() {
-  avatar.src = imagesArray[rand()]
+  avatar.src = imagesArray[rand()];
 }
 
 function resetAnimation() {
-  avatar.src = './assets/avatar.jpg'
+  avatar.src = "./assets/avatar.jpg";
+}
+
+function checkWidth() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth < 666) {
+    navigation.classList.add("asyde");
+  } else {
+    navigation.classList.remove("asyde");
+  }
+  codeChecker();
 }
 
 function closeMenu() {
-  navigation.classList.remove('asyde')
-  menu.removeAttribute('style')
-  menu.addEventListener('click', openMenu)
-  menu.removeEventListener('click', closeMenu)
+  const nav = document.querySelector("nav");
+  nav.style = "";
+  checkWidth();
+  menu.removeAttribute("style");
+  menu.addEventListener("click", openMenu);
+  menu.removeEventListener("click", closeMenu);
 }
 
 function openMenu() {
-  navigation.classList.add('asyde')
-  menu.setAttribute('style', 'background-image: url("./assets/close.svg")')
-  menu.removeEventListener('click', openMenu)
-  menu.addEventListener('click', closeMenu)
+  const nav = document.querySelector("nav");
+  nav.style = "display: block";
+  checkWidth();
+  menu.setAttribute("style", 'background-image: url("./assets/close.svg")');
+  menu.removeEventListener("click", openMenu);
+  menu.addEventListener("click", closeMenu);
 }
 
-avatar.addEventListener('mouseover', changeAvatar)
-avatar.addEventListener('mouseout', resetAnimation)
-navigation.children[0].addEventListener('click', (e) => {
-  const text = e.target.textContent.toLowerCase()
-  const target = document.getElementById(text)
-  target.setAttribute('style', 'background-color: yellow')
+avatar.addEventListener("mouseover", changeAvatar);
+avatar.addEventListener("mouseout", resetAnimation);
+menu.addEventListener("click", openMenu);
+window.addEventListener("resize", checkWidth);
+window.addEventListener("DOMContentLoaded", codeChecker);
+navigation.children[0].addEventListener("click", (e) => {
+  const text = e.target.textContent.toLowerCase();
+  const target = document.getElementById(text);
+  target.setAttribute("style", "background-color: yellow");
   setTimeout(() => {
-    target.removeAttribute('style')
+    target.removeAttribute("style");
   }, 3000);
-})
-menu.addEventListener('click', openMenu)
+});
